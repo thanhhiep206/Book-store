370935a52f114168f53a9da562a6b5257da74140
dth2k1 <gblhiep@gmail.com>
2022-05-13 22:03:57 +0700

    cartgory

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,14 +4,14 @@ const Book = require('../models/bookModel');
 const ReviewBook = require('../models/reviewBookModel');
 //render home
 exports.getIndex = catchAsync(async (req, res, next) => {
-  // const result = await axios({
-  //   method: 'get',
-  //   url: 'http://localhost:3000/api/v1/books',
-  // });
-  // const book = result.data.book;
-  const books = await Book.find();
+  const bookbestSale = await Book.find({ percentSale: { $gte: 35 } });
+
+  const bookProgram = await Book.find({ cartgory: 'laptrinh' });
+  const books = await Book.find({ percentSale: { $gte: 45 } });
   res.status(200).render('index', {
     title: 'LoveBook store for everyone',
+    bookbestSale,
+    bookProgram,
     books,
     user: req.user,
     style: 'index',
@@ -48,12 +48,15 @@ exports.getReview = catchAsync(async (req, res) => {
   //   fields: 'publishDate sizeWidth sizeHeight publisher coverImage pageNumber weight',
   // });
   const book = await Book.findOne({ slug: req.params.slug }).populate('reviews');
+  // book similar not equal above book
+  const bookSimilar = await Book.find({ cartgory: book.cartgory, slug: { $ne: req.params.slug } }).limit(5);
   // const reviewOfoneBook = book.reviews;
   const reviewOfoneBook = book.reviews;
   res.status(404).render('review', {
-    title: book.name,
+    title: 'Love book',
     user: req.user,
     book,
+    bookSimilar,
     reviewOfoneBook,
     style: 'product',
   });
@@ -65,7 +68,7 @@ exports.getCartgory = catchAsync(async (req, res) => {
   const books = await Book.find({ cartgory: req.params.cartgory });
   console.log(books);
   res.status(404).render('cartgory', {
-    title: 'ok',
+    title: 'Danh mục sách',
     books,
     user: req.user,
     style: 'product',
