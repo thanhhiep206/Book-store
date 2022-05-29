86273dc501eb2be4d0088ca226c3c28cf557e0ef
dth2k1 <gblhiep@gmail.com>
2022-05-29 17:40:18 +0700

    logout

--- public/js/loginForm.js
+++ public/js/loginForm.js
@@ -1,6 +1,5 @@
-console.log(1);
 const loginForm = document.querySelector('#form-signin');
-const logoutBtn = document.getElementById('logout');
+
 const login = (email, password) => {
   axios({
     method: 'post',
@@ -31,24 +30,3 @@ loginForm.addEventListener('submit', (e) => {
   const password = document.querySelector('#password').value;
   login(email, password);
 });
-// logout
-const logout = () => {
-  console.log(1);
-  axios({
-    method: 'get',
-    url: 'http://localhost:3000/api/v1/users/logout',
-  })
-    .then((res) => {
-      try {
-        if (res.data.status === 'success') {
-          location.reload(true);
-        }
-      } catch (e) {
-        console.log(e.response.data);
-      }
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-};
-logoutBtn.addEventListener('click', logout);
---
+++ public/js/logout.js
@@ -0,0 +1,21 @@
+const logoutBtn = document.getElementById('logout');
+const logout = () => {
+  console.log(1);
+  axios({
+    method: 'get',
+    url: 'http://localhost:3000/api/v1/users/logout',
+  })
+    .then((res) => {
+      try {
+        if (res.data.status === 'success') {
+          location.reload(true);
+        }
+      } catch (e) {
+        console.log(e.response.data);
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+logoutBtn.addEventListener('click', logout);
